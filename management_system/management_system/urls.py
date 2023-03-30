1b493659064381d1992a7c6cb80212e2d7fa2e72
Vanshika Sharma <108121341+vanshikasharma87@users.noreply.github.com>
2023-03-31 01:11:35 +0530

    Update urls.py

--- management_system/management_system/urls.py
+++ management_system/management_system/urls.py
@@ -17,17 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from django.views.static import serve
-from django.conf.urls import url
 
 from management_system import views, admin_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', views.BASE, name='base'),
-     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-    
 
     # login path
     path('', views.LOGIN, name='login'),
